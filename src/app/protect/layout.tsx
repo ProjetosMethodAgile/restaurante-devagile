83f6f16586f07d3bbe7c8f00f0b5c77ed2c6c3cc
otaviosouza21 <otaviosouzalu@gmail.com>
2025-05-09 20:13:41 -0300

    atualizacao

--- src/app/protect/layout.tsx
+++ src/app/protect/layout.tsx
@@ -7,10 +7,8 @@ export default async function ProtectedEmpresaLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-dvh overflow-hidden">
-      <header className="animate-move-right-to-left">
-        <NavigationMenu />
-      </header>
+    <div className="">
+      <NavigationMenu />
       <div className="h-[100vh] w-[100vw] overflow-auto">{children}</div>
     </div>
   );

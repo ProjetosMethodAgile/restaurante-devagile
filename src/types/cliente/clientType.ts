735f59f7c174d75b73ec8bc1ba71c84e755432dc
rafaelrvs <rafaelvieira1333@gmail.com>
2025-05-30 15:47:57 -0300

    melhorando client

---
+++ src/types/cliente/clientType.ts
@@ -0,0 +1,11 @@
+export type FormClienteData = {
+  contato: string;
+  nome: string;
+  cep: string;
+  logradouro: string;
+  numero: string;
+  complemento: string;
+  bairro: string;
+  frete: string;
+  observacao:string;
+};
